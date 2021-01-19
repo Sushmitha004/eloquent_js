e3197ad7c85820a06359f8d7801f026eb1f97c69
H S Sushmitha <sushmithahs004@gmail.com>
2021-01-19 12:32:40 +0530

    Added chapter-wise solutions

--- chap_04/reversing_an_array.js
+++ chap_04/reversing_an_array.js
@@ -1,19 +1,24 @@
 function reverseArray (array) {
   const arr = []
   for (let i = array.length - 1; i >= 0; i--) {
-    arr.unshift(array[i])
+    arr.push(array[i])
   }
   return arr
 }
 
-function reverseArrayInPlace () {
-  let arrInPlace = []
-  return arrInPlace
+function reverseArrayInPlace (array) {
+  let temp = 0
+  for (let i = 0, n = array.length; i < Math.floor(n / 2); i++) {
+    temp = array[i]
+    array[i] = array[n - 1 - i]
+    array[n - 1 - i] = temp
+  }
+  return array
 }
 
 console.log(reverseArray(['A', 'B', 'C']))
 // → ["C", "B", "A"];
-let arrayValue = [1, 2, 3, 4, 5]
+const arrayValue = [1, 2, 3, 4, 5]
 reverseArrayInPlace(arrayValue)
 console.log(arrayValue)
 // → [5, 4, 3, 2, 1]
